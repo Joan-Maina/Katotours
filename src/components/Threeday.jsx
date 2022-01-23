28e85afeb5bce5b656374acd0d90c1e3bda1a59b
Joan-Maina <mainajoan12@gmail.com>
2022-01-23 21:21:34 +0300

    add styling

--- src/components/Threeday.jsx
+++ src/components/Threeday.jsx
@@ -35,7 +35,51 @@ function Threeday() {
             </p>
           </div>
           <div className="elephants">
-            <Button style={style} text="YOUR ADVENTURE BEGINS" />
+            <div className="adventure">
+              <Button style={style} text="YOUR ADVENTURE BEGINS" />
+              <div className="adventureimage">
+                <div className="elephantimage">
+                  <i
+                    className="fa fa-chevron-left"
+                    id="gosideleft"
+                    aria-hidden="true"
+                  ></i>
+                  <div className="elephantsmasai">
+                    <img
+                      src="https://images.unsplash.com/photo-1481464904474-a575a33b44a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
+                      alt="elephants"
+                    />
+                  </div>
+
+                  <i
+                    className="fa fa-chevron-right"
+                    id="gosideright"
+                    aria-hidden="true"
+                  ></i>
+                </div>
+              </div>
+              <h5>1 Night in Location/Destinatiom</h5>
+              <p>
+                Description: Pretium, nunc, adipiscing pellentesque viverra
+                cursus at faucibus. Risus pellentesque montes, sit facilisis
+                lorem placerat ultricies donec dignissim. Praesent facilisi
+                iaculis gravida purus a, at eu risus nam. Lectus non eget
+                pharetra, eu, nibh condimentum. Dictumst lobortis nisl nunc,
+                ornare enim morbi pulvinar turpis. Eget elit est aliquet nunc
+                diam sollicitudin dis.
+              </p>
+              <p>
+                <b>Accomodation</b> : Lorem ipsum
+              </p>
+              <p>
+                <b>Meals</b> : Lorem ipsum
+              </p>
+            </div>
+          </div>
+          <div className="titletwo">
+            <p>Day 2: CONTINUE YOUR JOURNEY TO...</p>
+          </div>
+          <div className="adventure">
             <div className="adventureimage">
               <div className="elephantimage">
                 <i
@@ -45,7 +89,48 @@ function Threeday() {
                 ></i>
                 <div className="elephantsmasai">
                   <img
-                    src="https://images.unsplash.com/photo-1481464904474-a575a33b44a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
+                    src="https://images.unsplash.com/photo-1616128417743-c3a6992a65e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80"
+                    alt="elephants"
+                  />
+                </div>
+
+                <i
+                  className="fa fa-chevron-right"
+                  id="gosideright"
+                  aria-hidden="true"
+                ></i>
+              </div>
+            </div>
+            <h5>1 Night in Location/Destinatiom</h5>
+            <p>
+              Description: Pretium, nunc, adipiscing pellentesque viverra cursus
+              at faucibus. Risus pellentesque montes, sit facilisis lorem
+              placerat ultricies donec dignissim. Praesent facilisi iaculis
+              gravida purus a, at eu risus nam. Lectus non eget pharetra, eu,
+              nibh condimentum. Dictumst lobortis nisl nunc, ornare enim morbi
+              pulvinar turpis. Eget elit est aliquet nunc diam sollicitudin dis.
+            </p>
+            <p>
+              <b>Accomodation</b> : Lorem ipsum
+            </p>
+            <p>
+              <b>Meals</b> : Lorem ipsum
+            </p>
+          </div>{" "}
+          <div className="titletwo">
+            <p>Day 4: CONTINUE YOUR JOURNEY TO...</p>
+          </div>
+          <div className="adventure">
+            <div className="adventureimage">
+              <div className="elephantimage">
+                <i
+                  className="fa fa-chevron-left"
+                  id="gosideleft"
+                  aria-hidden="true"
+                ></i>
+                <div className="elephantsmasai">
+                  <img
+                    src="https://images.unsplash.com/photo-1550591853-18e1d18765e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=773&q=80"
                     alt="elephants"
                   />
                 </div>
@@ -73,84 +158,6 @@ function Threeday() {
               <b>Meals</b> : Lorem ipsum
             </p>
           </div>
-          <div className="titletwo">
-            <p>Day 2: CONTINUE YOUR JOURNEY TO...</p>
-          </div>
-          <div className="adventureimage">
-            <div className="elephantimage">
-              <i
-                className="fa fa-chevron-left"
-                id="gosideleft"
-                aria-hidden="true"
-              ></i>
-              <div className="elephantsmasai">
-                <img
-                  src="https://images.unsplash.com/photo-1481464904474-a575a33b44a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
-                  alt="elephants"
-                />
-              </div>
-
-              <i
-                className="fa fa-chevron-right"
-                id="gosideright"
-                aria-hidden="true"
-              ></i>
-            </div>
-          </div>
-          <h5>1 Night in Location/Destinatiom</h5>
-          <p>
-            Description: Pretium, nunc, adipiscing pellentesque viverra cursus
-            at faucibus. Risus pellentesque montes, sit facilisis lorem placerat
-            ultricies donec dignissim. Praesent facilisi iaculis gravida purus
-            a, at eu risus nam. Lectus non eget pharetra, eu, nibh condimentum.
-            Dictumst lobortis nisl nunc, ornare enim morbi pulvinar turpis. Eget
-            elit est aliquet nunc diam sollicitudin dis.
-          </p>
-          <p>
-            <b>Accomodation</b> : Lorem ipsum
-          </p>
-          <p>
-            <b>Meals</b> : Lorem ipsum
-          </p>
-          <div className="titletwo">
-            <p>Day 4: CONTINUE YOUR JOURNEY TO...</p>
-          </div>
-          <div className="adventureimage">
-            <div className="elephantimage">
-              <i
-                className="fa fa-chevron-left"
-                id="gosideleft"
-                aria-hidden="true"
-              ></i>
-              <div className="elephantsmasai">
-                <img
-                  src="https://images.unsplash.com/photo-1481464904474-a575a33b44a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
-                  alt="elephants"
-                />
-              </div>
-
-              <i
-                className="fa fa-chevron-right"
-                id="gosideright"
-                aria-hidden="true"
-              ></i>
-            </div>
-          </div>
-          <h5>1 Night in Location/Destinatiom</h5>
-          <p>
-            Description: Pretium, nunc, adipiscing pellentesque viverra cursus
-            at faucibus. Risus pellentesque montes, sit facilisis lorem placerat
-            ultricies donec dignissim. Praesent facilisi iaculis gravida purus
-            a, at eu risus nam. Lectus non eget pharetra, eu, nibh condimentum.
-            Dictumst lobortis nisl nunc, ornare enim morbi pulvinar turpis. Eget
-            elit est aliquet nunc diam sollicitudin dis.
-          </p>
-          <p>
-            <b>Accomodation</b> : Lorem ipsum
-          </p>
-          <p>
-            <b>Meals</b> : Lorem ipsum
-          </p>
         </div>
         <Formandcomments />
       </div>
